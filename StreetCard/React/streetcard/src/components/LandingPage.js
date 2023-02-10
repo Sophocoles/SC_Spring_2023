264d8cb9d3460e9e7e5631489b263f0fee2c54a7
Sophocoles <mhowar19@asu.edu>
2023-02-10 11:55:14 -0700

    Step 2
    pushed from pc, Will test on my laptop, delete excess files, then push to main

--- StreetCard/React/streetcard/src/components/LandingPage.js
+++ StreetCard/React/streetcard/src/components/LandingPage.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './LandingPage.css';
+//import './LandingPage.css';
 
-const LandingPage = () => {
+function LandingPage() {
   return (
-    <div className="landing-page">
-      <h1>Welcome to StreetCard</h1>
-      <Link to="/patients">
-        <button className="large-center-button">FHIR</button>
-      </Link>
+    <div>
+      <button className="large-center-button">FHIR</button>
       <button className="large-center-button">Old tier 1</button>
       <button className="large-center-button">Current landing page</button>
       <button className="large-center-button">Button 4</button>
     </div>
   );
-};
+}
 
 export default LandingPage;
