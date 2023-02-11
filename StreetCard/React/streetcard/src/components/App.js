b481b213789b50b9e001d360c12eccd7027273ba
Sophocoles <mhowar19@asu.edu>
2023-02-10 22:03:09 -0700

    added FHIR react example code
    from this repo
    
    https://github.com/Unite-Genomics/react-fhir-sample-app

--- StreetCard/React/streetcard/src/components/App.js
+++ StreetCard/React/streetcard/src/components/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import LandingPage from './LandingPage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Launcher from "./Launcher";
+import Redirect from "./Redirect";
 
-function App() {
-  return (
-    <div className="App">
-      <LandingPage />
-    </div>
-  );
+console.log("Inside app"); 
+
+export default function App() {
+    return (
+        <div>
+            <BrowserRouter>
+              <Routes>
+                <Route path='/redirect' element={<Redirect/>} />
+                <Route path='/' element={<Launcher/>} />
+              </Routes>
+                
+            </BrowserRouter>
+        </div>
+    );
 }
-
-export default App;
