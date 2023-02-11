4f15fd1ccb3a4c518e6a2a9fe7bb490bc46323eb
Sophocoles <58570720+Sophocoles@users.noreply.github.com>
2023-02-11 09:07:41 -0700

    Once again added landing page which links to different sections
    FHIR React app contains sample code

--- StreetCard/React/streetcard/src/components/App.js
+++ StreetCard/React/streetcard/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Launcher from "./Launcher";
 import Redirect from "./Redirect";
+import LandingPage from "./LandingPage";
 
 console.log("Inside app"); 
 
@@ -11,7 +12,8 @@ export default function App() {
             <BrowserRouter>
               <Routes>
                 <Route path='/redirect' element={<Redirect/>} />
-                <Route path='/' element={<Launcher/>} />
+                <Route path='/Launcher' element={<Launcher/>} />
+                <Route path='/' element={<LandingPage/>} />
               </Routes>
                 
             </BrowserRouter>
--- StreetCard/React/streetcard/src/components/LandingPage.js
+++ StreetCard/React/streetcard/src/components/LandingPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
   return (
     <div>
-      <Link to="/mycomponent"><button className="large-center-button">FHIR</button></Link>
+      <Link to="/Launcher"><button className="large-center-button">FHIR React App</button></Link>
       <button className="large-center-button">Old tier 1</button>
       <button className="large-center-button">Current landing page</button>
       <button className="large-center-button">Button 4</button>
