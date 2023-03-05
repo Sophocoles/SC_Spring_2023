20588a4a9adef6475dc1173acf73ef606f185e27
Allan DeBoe <adeboe@asu.edu>
2023-03-04 19:07:47 -0700

    (Task 222) Added myself as a superuser and created base html page
    While the base HTML page is not complete yet, at least it is there, and I had to modify Django to allow Wagtail to use the "Wagtail" directory of the project. Other than that, I have added myself as a superuser to be able to see the Wagtail admin page (under "localhost:8000/cms").

--- StreetCard/Django/StreetCard/StreetCard/settings.py
+++ StreetCard/Django/StreetCard/StreetCard/settings.py
@@ -138,7 +138,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../../Wagtail/pages'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -153,3 +157,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Wagtail Site Name
 # https://docs.wagtail.org/en/stable/getting_started/integrating_into_django.html
 WAGTAIL_SITE_NAME = 'StreetCard Project' # Change this to whatever fits the project
+
+WAGTAILADMIN_BASE_URL = '/cms/'
